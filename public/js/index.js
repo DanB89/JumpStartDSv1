be0026bc153f207069968e810af0a170a61c066e
Dan Bedi <danbedi@gmail.com>
2019-06-23 15:35:44 +0100

    completed it

--- public/js/index.js
+++ public/js/index.js
@@ -1,91 +1,72 @@
 window.onload = function(){
+  var button = document.getElementById("button")
+  button.addEventListener("click", makeRequest);
+  var cityInput = document.getElementById("cityInput");
 
-var title = document.getElementById("button2");
 
-console.log(button2);
-
-title.style.color = "blue";
-
+cityInput.addEventListener("keydown", function(e){
+  if (e.keyCode === 13) {
+    makeRequest();
+  }
+})
 }
 
 
 
 
+function makeRequest (){
+  var cityInput = document.getElementById("cityInput");
+  console.log(cityInput);
 
+  var apiKey = "ad5d84d8e7a3ae5d22885073b250ca14"
+  var weathermain = document.getElementById("weathermain")
+  var weatherhumidity = document.getElementById("weatherhumidity")
+  var weatherdescription = document.getElementById("weatherdescription")
+  var weathertemperature = document.getElementById("weathertemperature")
+  var weatherimage = document.getElementById("weatherimage")
 
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" +cityInput.value +"&APPID=ad5d84d8e7a3ae5d22885073b250ca14", true);
+  xhr.onload = function (e){
+  if(xhr.readyState ===4){
+    if(xhr.status === 200){
 
+      var data = JSON.parse(xhr.responseText);
+      console.log(data);
+      weathermain.innerHTML = data.weather[0].main
+      console.log(data.weather[0]);
 
+      weatherhumidity.innerHTML = data.main.humidity + "%"
+      console.log(data.weather[0]);
 
+      weatherdescription.innerHTML = data.weather[0].description
+      console.log(data.weather[0]);
 
+      weathertemperature.innerHTML = (data.main.temp-273.15).toFixed() + "˚C"
+      console.log(data.weather[0]);
 
-
-
-
-
-
-
-
-  cityInput.addEventListener('keydown', function(e){
-    if (e.keyCode === 13) {
-      makeRequest();
-    }
-  })
-
-  button.addEventListener('click', makeRequest);
-
-  function makeRequest() {
-    var weathermain = document.getElementById('weathermain')
-    var weatherdescription = document.getElementById('weatherdescription')
-    var weathertemperature = document.getElementById('weathertemperature')
-    var weatherhumidity = document.getElementById('weatherhumidity')
-    var weatherimage = document.getElementById('image')
-    var button = document.getElementById('button')
-    var title = document.getElementById('title')
-    var inputcontainer = document.getElementById('inputcontainer')
-    var cityInput = document.getElementById('cityInput')
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" +cityInput.value +"&APPID=91a5954de67a5f9f37db0d87b7e1c82e", true);
-    xhr.onload = function (e){
-      if(xhr.readyState === 4){
-        if(xhr.status === 200){
-
-          var data = JSON.parse(xhr.responseText);
-          weathermain.innerHTML = data.weather[0].main
-
-          if (daytime) {
-            if (data.weather[0].main === "Clear") {
-              weatherimage.src="../../assets/images/clear.png"
-            }
-            if (data.weather[0].main === "Clouds") {
-              weatherimage.src="../../assets/images/clouds.png"
-            }
-            if (data.weather[0].main === "Rain") {
-              weatherimage.src="../../assets/images/rain.png"
-            }
-            if (data.weather[0].main === "Drizzle") {
-              weatherimage.src="../../assets/images/rain.png"
-            }
-            if (data.weather[0].main === "Snow") {
-              weatherimage.src="../../assets/images/snowflake.png"
-            }
-          } else {
-
-          }
-
-          console.log(data.weather[0].main);
-
-          weatherdescription.innerHTML = toTitleCase(data.weather[0].description)
-          weathertemperature.innerHTML = (data.main.temp - 273.15).toFixed() +"°C"
-          weatherhumidity.innerHTML = data.main.humidity +"%"
-
-        } else {
-          console.error(xhr.statusText)
-        }
+      if (data.weather[0].main === "Clear") {
+        weatherimage.src="../../assets/images/clear.png"
       }
-    };
-    xhr.onerror = function (e) {
-      console.error(xhr.statusText);
-    };
-    xhr.send(null)
-  }
+      if (data.weather[0].main === "Clouds") {
+        weatherimage.src="../../assets/images/clouds.png"
+      }
+      if (data.weather[0].main === "Rain") {
+        weatherimage.src="../../assets/images/rain.png"
+      }
+      if (data.weather[0].main === "Drizzle") {
+        weatherimage.src="../../assets/images/rain.png"
+      }
+      if (data.weather[0].main === "Snow") {
+        weatherimage.src="../../assets/images/snowflake.png"
+      }
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null)
+}
